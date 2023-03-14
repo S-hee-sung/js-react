34c384edcd65db3cbec147205a4ad0a31d3735e2
S-hee-sung <dlsglrdhws@naver.com>
2023-03-14 09:46:49 +0900

    11

--- 12_typescript/1_types.ts
+++ 12_typescript/1_types.ts
@@ -201,18 +201,18 @@ let userInfo: any = [userName, userAge, married];
 
 // Q5. frontClass라는 변수에 타입을 지정해보세요.
 let frontClass: {
-  subject: string[],
+  subject: any,
   score: number[],
   teacher: string,
-  student: string
+  student: string | string[]
 } = {
   subject: ['html', 'css', 'js', 'react'],
   score: [100, 95, 85, 90],
   teacher: 'Kim',
   student: 'Lee'
 };
-// frontClass.subject[5] = false;
-// frontClass.student = ['Lee', 'Park'];
+frontClass.subject[5] = false;
+frontClass.student = ['Lee', 'Park'];
 // 타입 지정을 안해주면 터미널에 에러 발생
 // 에러가 안나게 frontClass라는 변수에 타입 지정
 
